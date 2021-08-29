bcb7c250c4f324cfe77d9ed5ba998240d044da71
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-08-29 15:52:22 +0200

    New order in scss folder + hero done!

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,19 @@
 // import * as classes from '../scss/main.scss'; // For css in js
+
+// Theme toggle
+const body = document.querySelector("body");
+const themeToggle = document.querySelector("#themeToggle");
+
+themeToggle.addEventListener("click", toggleTheme)
+
+function toggleTheme() {
+  if (body.classList.contains("theme-light")) {
+    body.classList.replace("theme-light", "theme-dark");
+    toDark.classList.toggle("hide");
+    toLight.classList.toggle("hide");
+  } else {
+    body.classList.replace("theme-dark", "theme-light");
+    toLight.classList.toggle("hide");
+    toDark.classList.toggle("hide");
+  }
+}
