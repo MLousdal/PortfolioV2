c96173e89a1271c3f9fbe801dabb481f1649a43b
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-08-30 23:25:09 +0200

    Added animations
    Ready for build!

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -20,3 +20,22 @@ themeToggle.addEventListener("click", () => {
     body.classList.replace("theme-dark", "theme-light");
   }
 });
+
+
+// Animations
+import "animate.css";
+
+const animateMe = document.querySelectorAll(".animateMe");
+const windowHeight = window.innerHeight;
+
+body.onscroll = checkPosition;
+
+function checkPosition() {
+  animateMe.forEach(e => {
+    let positionFromTop = e.getBoundingClientRect().top;
+
+    if (positionFromTop - windowHeight <= 0) {
+      e.classList.add('animate__animated', 'animate__fadeInUp');
+    };
+  });
+};
