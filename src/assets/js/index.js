f86d64e59ea7c6f31272bd917875f5aca50e9a84
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-08-30 22:26:43 +0200

    Fixed links + added switch as theme toggle

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,19 +1,22 @@
 // import * as classes from '../scss/main.scss'; // For css in js
 
+// Scroll arrow
+
+const arrow = document.querySelector("#arrow");
+const cases = document.querySelector("#cases");
+
+arrow.addEventListener("click", () => {
+  cases.scrollIntoView();
+});
+
 // Theme toggle
 const body = document.querySelector("body");
 const themeToggle = document.querySelector("#themeToggle");
 
-themeToggle.addEventListener("click", toggleTheme)
-
-function toggleTheme() {
+themeToggle.addEventListener("click", () => {
   if (body.classList.contains("theme-light")) {
     body.classList.replace("theme-light", "theme-dark");
-    toDark.classList.toggle("hide");
-    toLight.classList.toggle("hide");
   } else {
     body.classList.replace("theme-dark", "theme-light");
-    toLight.classList.toggle("hide");
-    toDark.classList.toggle("hide");
   }
-}
+});
